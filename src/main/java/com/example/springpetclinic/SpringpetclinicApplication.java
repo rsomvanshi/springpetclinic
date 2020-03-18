39f735b82e002692467cab2603ac3efcb0b5c324
Rohan Somvanshi <rsomvanshi@medallia.com>
2020-03-18 10:50:54 -0400

    Bootstrap project

---
+++ src/main/java/com/example/springpetclinic/SpringpetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetclinicA`pplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetclinicApplication.class, args);
+    }
+
+}
