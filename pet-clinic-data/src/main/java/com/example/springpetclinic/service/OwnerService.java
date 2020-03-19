d8bc964a3d8e8da6f613e9ba1bb24853bf2bc793
Rohan Somvanshi <rsomvanshi@medallia.com>
2020-03-19 12:59:21 -0400

    Add services for models

---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/service/OwnerService.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinic.service;
+
+import com.example.springpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findById(Long id);
+    Owner findByLastName(String lastName);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/example/springpetclinic/service/VetService.java
@@ -0,0 +1,12 @@
+package com.example.springpetclinic.service;
+
+import com.example.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet pet);
+    Set<Vet> findAll();
+}
