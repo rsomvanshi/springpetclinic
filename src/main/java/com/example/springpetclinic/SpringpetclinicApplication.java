a3a432371ebf31b5592922894616c6e1cbce3ad1
Rohan Somvanshi <rsomvanshi@medallia.com>
2020-03-19 08:41:44 -0400

    Add data model

--- src/main/java/com/example/springpetclinic/SpringpetclinicApplication.java
+++ src/main/java/com/example/springpetclinic/SpringpetclinicApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringpetclinicA`pplication {
+public class SpringpetclinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringpetclinicApplication.class, args);
